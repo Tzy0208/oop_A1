c9713972158a1a47b07b454994a4f4c63c9e8fb0
Tzy <1489354119>
2024-11-19 13:48:32 +0800

    A method for createApp was created in class AppoinmentManagement

--- username-A1/src/Appointment.java
+++ username-A1/src/Appointment.java
@@ -5,28 +5,27 @@ public class Appointment {
     String ptname;
     int phonenumber;
     String selectDoctor;
-    LocalTime selecttime;
+    LocalTime time;
 
     public  Appointment(){
         this.ptname=" ";
         this.phonenumber=0;
         this.selectDoctor=" ";
-        this.selecttime=LocalTime.of(0,0);
+        this.time=LocalTime.of(0,0);
     }
 
     public Appointment(String ptname,int phonenumber, String selectDoctor,LocalTime time){
          this.ptname = ptname;
          this.phonenumber=phonenumber;
          this.selectDoctor=selectDoctor;
-         this.selecttime=time;
+         this.time=time;
     }
     public void printAppinfo(){
         System.out.println("病人的的名字是"+":"+this.ptname);
         System.out.println("病人的的电话号码是"+":"+this.phonenumber);
         System.out.println("病人选择的医生是"+":"+this.selectDoctor);
-        System.out.println("病人选择的时间是"+":"+this.selecttime);
+        System.out.println("病人选择的时间是"+":"+this.time);
     }
     
 
-    }
 }
--- username-A1/src/AppointmentManagement.java
+++ username-A1/src/AppointmentManagement.java
@@ -1,6 +1,23 @@
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class AppointmentManagement extends Appointment{
-    public ArrayList<Appointment>appointments=new ArrayList<>();
+public class AppointmentManagement {
+    public ArrayList<Appointment> appointments;
+    public AppointmentManagement(){
+        appointments=new ArrayList<>();
+    }
+    public void createApp(String ptname,int phonenumber,String selectDoctor,LocalTime time){
+         if (ptname==null & phonenumber== 0 & selectDoctor==null & time==null){
+             System.out.println("没满足条件,无法创建appointment");
+         }
+         else{
+            Appointment appointment=new Appointment(ptname,phonenumber,selectDoctor,time);
+            appointments.add(appointment);
+         }
+
+    }
+    public void printExinfo(){
+        
+    }
 }
